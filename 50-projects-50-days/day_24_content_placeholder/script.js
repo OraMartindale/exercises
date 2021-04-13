885e1fcf5d4b450f34199221e52f1475ef5132ae
Ora Martindale <ora@martindale.org>
2021-04-13 16:45:08 -0500

    50 Projects in 50 Days' Day 24: Content Placeholder

---
+++ 50-projects-50-days/day_24_content_placeholder/script.js
@@ -0,0 +1,23 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+const getData = () => {
+  header.innerHTML = '<img src="https://images.unsplash.com/photo-1507764923504-cd90bf7da772?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="">';
+  title.innerHTML = 'Lorem ipsum dolor sit amet.';
+  excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, in?';
+  profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">';
+  name.innerHTML = 'John Doe';
+  date.innerHTML = 'Apr 13, 2021';
+
+  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+  animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'));
+};
+
+setTimeout(getData, 2500);
