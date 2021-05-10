5cb16a95d65c298f2f8ba4f82db35adca1ec71c5
Ora Martindale <ora@martindale.org>
2021-05-10 18:17:21 -0500

    Disney+ Clone: Add Categories with Firebase Db
    Added categories such as Recommended for You, New to Disney+, Originals
    and Trending. The data is stored in Firebase's Firestore Db.

--- disney-plus-clone/src/components/Home.js
+++ disney-plus-clone/src/components/Home.js
@@ -1,12 +1,70 @@
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Recommends from './Recommends';
+import Trending from './Trending';
 import Viewers from './Viewers';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisney = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    db.collection('movies').onSnapshot(snapshot => {
+      snapshot.docs.map(doc => {
+        switch (doc.data().type) {
+          case 'recommend':
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case 'new':
+            newDisney = [...newDisney, { id: doc.id, ...doc.data() }];
+            break;
+
+          case 'original':
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case 'trending':
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+
+          default:
+            break;
+        }
+
+        return '';
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisney,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
+
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
 };
